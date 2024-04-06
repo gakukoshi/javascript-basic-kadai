dea2130c851135e37967d6f2ff16c7276eb70e8b
gakukoshi <157940708+gakukoshi@users.noreply.github.com>
2024-04-06 15:31:56 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    
+ const text = document.getElementById('text');
+ text.textContent  = "ボタンをクリックしました";
+});
